bc1399ad4cb7c743c522987d4a29d1c9ad1bf0f4
Dmitriy Sukharev <dmitriy.pastanovskiy@gmail.com>
2024-07-04 17:58:54 +0300

    Initial commit

---
+++ DifferenceOfSquares.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Linq;
+
+public static class DifferenceOfSquares
+{
+    public static int CalculateSquareOfSum(int max) =>
+        (int)Math.Pow(Enumerable.Range(1, max).Sum(), 2);
+
+    public static int CalculateSumOfSquares(int max) =>
+        (int)Enumerable.Range(1, max).Select(digit => Math.Pow(digit, 2)).Sum();
+
+    public static int CalculateDifferenceOfSquares(int max) =>
+        CalculateSquareOfSum(max) - CalculateSumOfSquares(max);
+}
